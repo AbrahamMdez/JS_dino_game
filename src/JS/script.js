aaaf07917c254254dc7c65669643aa39e4edab3a
Abraham <nencho_1@hotmail.com>
2021-04-16 10:58:21 +0200

    done conditional to cactus and dino collision

--- src/JS/script.js
+++ src/JS/script.js
@@ -1,17 +1,30 @@
 'use strict';
 
-const dino = document.querySelector('#dino');
+const dino   = document.querySelector('#dino');
+const cactus = document.querySelector('#cactus');
 
 const jump = () => {
     if (dino.classList != 'jump') {
-        
+
         dino.classList.add('jump');
 
-        setTimeout(function () {
+        setTimeout(function() {
             dino.classList.remove('jump');
         }, 300);
     };
 };
+
+let isAlive = setInterval(function() {
+
+    let dinoTopPosition = parseInt(window.getComputedStyle(dino).getPropertyValue('top'));
+
+    let cactusLeftPosition = parseInt(window.getComputedStyle(cactus).getPropertyValue('left'));
+    
+    if (cactusLeftPosition < 50 && cactusLeftPosition > 0 && dinoTopPosition >= 140 ) {
+        console.log('collision');
+    }
+   
+}, 10);
 
 document.addEventListener('keydown', e => {
     jump();
