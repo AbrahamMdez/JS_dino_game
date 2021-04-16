aef3afab6c886f1861bbcfbf037f3bc7f03dd344
Abraham <nencho_1@hotmail.com>
2021-04-16 10:42:43 +0200

    finished function to cactus

--- src/JS/script.js
+++ src/JS/script.js
@@ -3,6 +3,16 @@
 const dino = document.querySelector('#dino');
 
 const jump = () => {
-    dino.classList.add('jump');
+    if (dino.classList != 'jump') {
+        
+        dino.classList.add('jump');
+
+        setTimeout(function () {
+            dino.classList.remove('jump');
+        }, 300);
+    };
 };
 
+document.addEventListener('keydown', e => {
+    jump();
+});
